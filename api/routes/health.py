5d94d1a45b542905b93b98b5a0974405e032778f
Tabish Ali Ansari <ansaritabishali1@gmail.com>
2026-03-24 00:33:56 +0530

    postgres db connect

--- api/routes/health.py
+++ api/routes/health.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 from services.health_service import get_health
+from db.database import check_db_connection
 
 router = APIRouter(prefix="/health", tags=["Health", "Status"])
 
 @router.get("/")
 def system_health():
     return get_health()
+
+
+@router.get("/db")
+def db_status():
+    return check_db_connection()
---
+++ db/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, text
+
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mydb"
+
+engine = create_engine(DATABASE_URL)
+
+def check_db_connection():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return {"database_status": "Connected to postgres"}
+    except Exception as e:
+        return {"database_status": "Not connected", "error": str(e)}
